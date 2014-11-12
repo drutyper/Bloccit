abd9029bc4d0920bb6f45e5b001f04787fd6ca05
dru <drutyper@gmail.com>
2014-11-12 11:36:45 -0500

    nesting post

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-   
+
   devise_for :users
   resources :posts
+  resources :topics
 
-  get 'about' => 'welcome#about'
 
  authenticated :user do
-   root to: 'posts#index', as: 'authenticated_root'
+   root to: 'topics#index', as: 'authenticated_root'
  end
 
   root to: 'welcome#index'

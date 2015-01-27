b7230c7842f76b39d1941d3a99cbdd3813e1e02b
Dru <drutyper@gmail.com>
2015-01-26 20:08:23 -0500

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'comments/create'
+
   devise_for :users
   resources :users, only: [:update]
    
    resources :topics do
-     resources :posts, except: [:index]
-   end
+   resources :posts, except: [:index]
+   resources :comments, only: [:create]
+ end
 
 
 get 'about' => 'welcome#about'

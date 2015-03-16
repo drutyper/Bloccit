4335cc923c9f74a54f485900c8327685245042ad
Dru <drutyper@gmail.com>
2015-03-16 18:44:59 -0400

    Mocking rspec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,9 @@ describe Post do
   describe "vote methods" do
 
     before do
-      @post = Post.create(title: 'post title', body: 'post bodies must be pretty long')
+      @post = Post.new(title: 'Post title', body: 'Post bodies must be pretty long.')
+        allow(@post).to receive(:create_vote)
+      @post.save
       3.times {@post.votes.create(value: 1) }
       2.times { @post.votes.create(value: -1) }
     end

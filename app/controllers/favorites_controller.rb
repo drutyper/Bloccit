0e38d7685d9df8315abdfa2e8f900a1e661459b1
Dru <drutyper@gmail.com>
2015-03-18 19:44:12 -0400

    Completed favorite checkpoint

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,11 +3,26 @@ class FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post: @post)
     authorize favorite
+    
     if favorite.save
       flash[:notice] = "Favorited post"
       redirect_to [@post.topic, @post]
     else
       flash[:error] = "Unable to add favorite. Please try again."
+      redirect_to @post
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+
+    if favorite.destroy
+      flash[:notice] = "Post was unfavorited."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "Unable to unfavorite post."
       redirect_to [@post.topic, @post]
     end
   end

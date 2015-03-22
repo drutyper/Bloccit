fa0e34c2310a68a00a46532f665e8fc3916a029e
Dru <drutyper@gmail.com>
2015-03-22 09:41:00 -0400

    Another interlude

--- Gemfile
+++ Gemfile
@@ -14,6 +14,12 @@ group :development do
    gem 'sqlite3'
    gem 'byebug'
  end
+
+group :test do 
+  gem 'capybara'
+end
+
+
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,15 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError do |exception|
      redirect_to root_url, alert: exception.message
-   end
+  end
+
+  def after_sign_in_path_for(resources)
+    topics_path
+  end
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
+    end
 end

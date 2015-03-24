868e9159b27f32204705a3dd5fe22615a79fc909
Dru <drutyper@gmail.com>
2015-03-24 10:48:40 -0400

    public profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,25 @@
- class UsersController < ApplicationController
-   before_action :authenticate_user!
+class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
  
-   def update
-     if current_user.update_attributes(user_params)
-       flash[:notice] = "User information updated"
-       redirect_to edit_user_registration_path
-     else
-       flash[:error] = "Invalid user information"
-       redirect_to edit_user_registration_path
-     end
-   end
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "Invalid user information"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @comments = @user.comments
+  end
  
-   private
+  private
  
    def user_params
      params.require(:user).permit(:name, :avatar, :email_favorites)
    end
- end
+end
